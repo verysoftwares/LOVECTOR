defdc169063d1390725897f793cb261167b79f59
verysoftwares <54413321+verysoftwares@users.noreply.github.com>
2023-04-08 18:05:58 +0300

    main file linking

--- main.lua
+++ main.lua
@@ -1,11 +1,4 @@
 -- file linking.
-    require 'load'
-    require 'alias'
-    require 'utility'
     require 'update'
     require 'draw'
     require 'bezier'
-
-t = 0
--- runtime.
-    print(fmt('made by verysoftwares with LOVE %d.%d', love.getVersion()))
