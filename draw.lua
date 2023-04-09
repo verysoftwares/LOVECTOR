562b45328c14c8ab96fc539630e69c6b43bc44d6
verysoftwares <54413321+verysoftwares@users.noreply.github.com>
2023-04-09 17:32:17 +0300

    no need for 'if t>=' expressions

--- draw.lua
+++ draw.lua
@@ -4,22 +4,15 @@ function draw()
     love.graphics.setColor(0.4,0.4,0.2)
     love.graphics.setLineWidth(5)
 
-    if t>=0 and t<30*4 then
-        -- single point
-        bezier(dot(200,100))
-    end
-    if t>=30 then
-        -- line
-        bezier(dot(100,100),dot(400,400),(t-30*4)*0.02,(t-30)*0.02)
-    end
-    if t>=30*2 then
-        -- curve
-        bezier(dot(100,100),dot(400,400),dot(400,100),(t-30*5)*0.02,(t-30*2)*0.02)
-    end
-    if t>=30*3 then
-        -- double curve
-        bezier(dot(100,100),dot(400,400),dot(400,100),dot(700,200),(t-30*6)*0.04,(t-30*3)*0.04)
-    end
+    -- single point
+    bezier(dot(200,100))
+    -- line
+    bezier(dot(100,100),dot(400,400),(t-30*4)*0.02,(t-30)*0.02)
+    -- curve
+    bezier(dot(100,100),dot(400,400),dot(400,100),(t-30*5)*0.02,(t-30*2)*0.02)
+    -- double curve
+    bezier(dot(100,100),dot(400,400),dot(400,100),dot(700,200),(t-30*6)*0.04,(t-30*3)*0.04)
+
     if t>=30*6+30 then t=0 end
 end
 
